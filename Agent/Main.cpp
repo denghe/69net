c34006340d2143594263275a01a76a775d525456
denghe <denghe@hotmail.com>
2014-07-27 07:34:41 +0800

    SMBuffer more test

--- Agent/Main.cpp
+++ Agent/Main.cpp
@@ -2,20 +2,39 @@
 
 int main()
 {
-    int bufLen = 32;
+    // 每 0.2 秒输出一次总 counter 及 每秒 count
+    std::chrono::milliseconds refreshDuration( 200 );
+    std::chrono::seconds durationSec1( 1 );
+    std::chrono::time_point<std::chrono::system_clock> lastTime;
+    auto firstTime = std::chrono::system_clock::now();
+    auto cout = [ &]( int64 val )
+    {
+        auto now = std::chrono::system_clock::now();
+        if( now - lastTime >= refreshDuration )
+        {
+            auto elapsedSeconds = std::chrono::duration_cast<std::chrono::seconds>( now - firstTime ).count();
+            if( !elapsedSeconds ) elapsedSeconds = 1;
+            CoutPos( 0, 0, val, ", ", val / elapsedSeconds );
+            lastTime = now;
+        }
+    };
+
+
+    int bufLen = 102400;
     SharedMemory::setPrefixName( "69" );
     auto& smb = *(SMBuffer*)SharedMemory::create( 123, bufLen );
     smb.init( bufLen );
 
-    int i = 0;
+    int64 counter = 0;
     while( true )
     {
         if( smb.space() )
         {
-            char buf = ++i % bufLen;
-            smb.write( &buf, 1 );
+            ++counter;
+            smb.write( (char*)&counter, sizeof( counter ) );
+            cout( counter );
         }
-        else std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
+        //else std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
     }
 
     system( "pause" );
--- Test_Lib/Main.cpp
+++ Test_Lib/Main.cpp
@@ -5,19 +5,33 @@ int main()
     // wait sm keeper( Agent project ) create sm
     std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
 
-    int bufLen = 32;
+    // 每 0.2 秒显示一次的 cout
+    std::chrono::milliseconds refreshDuration( 200 );
+    std::chrono::time_point<std::chrono::system_clock> lastTime;
+    auto cout = [ &]( int64 val )
+    {
+        auto now = std::chrono::system_clock::now();
+        if( now - lastTime >= refreshDuration )
+        {
+            CoutPos( 0, 0, val, ' ' );
+            lastTime = now;
+        }
+    };
+
+    int bufLen = 102400;
     SharedMemory::setPrefixName( "69" );
     auto& smb = *(SMBuffer*)SharedMemory::create( 123, bufLen );
 
+
+    int64 buf;
     while( true )
     {
-        char buf;
         if( smb.size() )
         {
-            smb.read( &buf, 1 );
-            Cout( (int)buf );
+            smb.read( (char*)&buf, sizeof( buf ) );
+            cout( buf );
         }
-        else std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
+        // else std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
     }
 
     system( "pause" );
