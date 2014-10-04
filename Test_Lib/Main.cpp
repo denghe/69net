3ac3ffd3c05eb8c6f1d1510ea460d90a2cd3ab7e
denghe <denghe@hotmail.com>
2014-10-04 14:33:24 +0800

    test

--- Test_Lib/Main.cpp
+++ Test_Lib/Main.cpp
@@ -9,44 +9,71 @@ using namespace std;
 
 int main()
 {
-    set<int> sss;
     {
+        set<int> sss;
         Stopwatch sw;
-        for( int i = 0; i < 9999999; ++i )
+        for( int i = 5555; i < 9999999; ++i )
         {
-            sss.insert( i );
-        }
-        Cout( sw.elapsedMillseconds() );
-        Cout( sss.size() );
-
-        sw.reset();
-        for( int i = 0; i < 9999999; ++i )
-        {
-            sss.erase( i );
+            auto oldval = ( i - 5555 ) % 9999;
+            auto newval = i % 9999;
+            sss.erase( oldval );
+            sss.insert( newval );
         }
         Cout( sw.elapsedMillseconds() );
         Cout( sss.size() );
     }
 
-    Set<int> myset;
     {
+        vector<int> vvv;
         Stopwatch sw;
-        myset.reserve( 9999999 );
-        for( int i = 0; i < 9999999; ++i )
+        for( int i = 5555; i < 9999999; ++i )
         {
-            myset.insert( i );
+            auto oldval = ( i - 5555 ) % 9999;
+            auto newval = i % 9999;
+            //vvv.erase( oldval );
+            auto it = find( vvv.begin(), vvv.end(), oldval );
+            if( it != vvv.end() ) vvv.erase( it );
+            vvv.push_back( newval );
         }
         Cout( sw.elapsedMillseconds() );
-        Cout( myset.size() );
-
-        sw.reset();
-        for( int i = 0; i < 9999999; ++i )
-        {
-            myset.erase( i );
-        }
-        Cout( sw.elapsedMillseconds() );
-        Cout( myset.size() );
+        Cout( vvv.size() );
     }
+
+    {
+        Set<int> sss;
+        Stopwatch sw;
+        for( int i = 5555; i < 9999999; ++i )
+        {
+            auto oldval = ( i - 5555 ) % 9999;
+            auto newval = i % 9999;
+            sss.erase( oldval );
+            sss.insert( newval );
+        }
+        Cout( sw.elapsedMillseconds() );
+        Cout( sss.size() );
+    }
+
+
+    //Set<int> myset;
+    //{
+    //    Stopwatch sw;
+    //    myset.reserve( 9999999 );
+    //    for( int i = 0; i < 9999999; ++i )
+    //    {
+    //        myset.insert( i );
+    //    }
+    //    Cout( sw.elapsedMillseconds() );
+    //    Cout( myset.size() );
+
+    //    sw.reset();
+    //    for( int i = 0; i < 9999999; ++i )
+    //    {
+    //        myset.erase( i );
+    //    }
+    //    Cout( sw.elapsedMillseconds() );
+    //    Cout( myset.size() );
+    //}
+
 
     system( "pause" );
     return 0;
