9678af3d926e2824ecc8a2e9ccce9df3aef9ea49
denghe <denghe@hotmail.com>
2014-10-13 17:14:58 +0800

    coroutine test

--- Test_Lib/Main.cpp
+++ Test_Lib/Main.cpp
@@ -1,212 +1,52 @@
-#include <iostream>
-#include <vector>
-#include <functional>
-#include <array>
-#include <memory>
-#include <cassert>
-#include <list>
 #include "Lib/All.h"
 using namespace std;
 
-struct StdFoo
-{
-    list<StdFoo*>::iterator it;
-};
+#define COR_BEGIN switch( labNum ) { case 0:
+#define COR_YIELD_RETURN labNum = __LINE__; return true; case __LINE__:
+#define COR_END } return false;
 
-struct MyFoo
+struct Foo
 {
-    Links<MyFoo*>::Node* it;
+    int i;
+    int idx = 0;
+    int labNum = 0;
+    bool process()
+    {
+        COR_BEGIN;
+        for( i = 0; i < 10; ++i )
+        {
+            Cout( i );
+            COR_YIELD_RETURN;
+        }
+        COR_END;
+    }
 };
 
 int main()
 {
-    vector<StdFoo*> stdfoos;
-    stdfoos.reserve( 9999999 );
-    vector<MyFoo*> myfoos;
-    myfoos.reserve( 9999999 );
-
-    for( int x = 0; x < 10; ++x )
+    List<Foo*> foos;
+    for( int i = 0; i < 3; ++i )
     {
-
-        {
-            int count = 0;
-            list<StdFoo*> c;
-            Stopwatch sw;
-            for( int i = 0; i < 9999999; ++i )
-            {
-                auto foo = new StdFoo();
-                stdfoos.push_back( foo );
-                c.push_back( foo );
-                foo->it = --c.end();
-            }
-            Cout( "std::list pus_back ms: ", sw.elapsedMillseconds() );
-            Cout( c.size() );
-
-            sw.reset();
-            for( int i = 0; i < 9999999; ++i )
-            {
-                c.erase( stdfoos[ i ]->it );
-                delete stdfoos[ i ];
-            }
-            Cout( "std::list erase ms: ", sw.elapsedMillseconds() );
-
-            stdfoos.clear();
-        }
-
-        {
-            int count = 0;
-            set<StdFoo*> c;
-            Stopwatch sw;
-            for( int i = 0; i < 9999999; ++i )
-            {
-                auto foo = new StdFoo();
-                stdfoos.push_back( foo );
-                c.insert( foo );
-            }
-            Cout( "std::set insert ms: ", sw.elapsedMillseconds() );
-            Cout( c.size() );
-
-            sw.reset();
-            for( int i = 0; i < 9999999; ++i )
-            {
-                c.erase( stdfoos[ i ] );
-                delete stdfoos[ i ];
-            }
-            Cout( "std::set erase ms: ", sw.elapsedMillseconds() );
-
-            stdfoos.clear();
-        }
-
-        {
-            int count = 0;
-            Links<MyFoo*> c;
-            Stopwatch sw;
-            for( int i = 0; i < 9999999; ++i )
-            {
-                auto foo = new MyFoo();
-                myfoos.push_back( foo );
-                foo->it = c.insert( foo );
-            }
-            Cout( "Links pus_back ms: ", sw.elapsedMillseconds() );
-            Cout( c.size() );
-
-            sw.reset();
-            for( int i = 0; i < 9999999; ++i )
-            {
-                c.erase( myfoos[ i ]->it );
-                delete myfoos[ i ];
-            }
-            Cout( "Links erase ms: ", sw.elapsedMillseconds() );
-
-            myfoos.clear();
-        }
-
-        {
-            int count = 0;
-            Hash<MyFoo*> c;
-            Stopwatch sw;
-            for( int i = 0; i < 9999999; ++i )
-            {
-                auto foo = new MyFoo();
-                myfoos.push_back( foo );
-                c.insert( foo );
-            }
-            Cout( "Hash pus_back ms: ", sw.elapsedMillseconds() );
-            Cout( c.size() );
-
-            sw.reset();
-            for( int i = 0; i < 9999999; ++i )
-            {
-                c.erase( myfoos[ i ] );
-                delete myfoos[ i ];
-            }
-            Cout( "Hash erase ms: ", sw.elapsedMillseconds() );
-
-            myfoos.clear();
-        }
-
-        Cout( "\n\n" );
+        auto foo = new Foo();
+        foo->idx = foos.size();
+        foos.push( foo );
     }
-
-    //int count;
-    //for( int x = 0; x < 10; ++x )
-    //{
-    //    {
-    //        list<int> c;
-    //        Stopwatch sw;
-    //        for( int i = 0; i < 9999999; ++i )
-    //        {
-    //            c.push_back( i );
-    //        }
-    //        Cout( "std::list pus_back ms: ", sw.elapsedMillseconds() );
-    //        Cout( c.size() );
-
-    //        count = 0;
-    //        sw.reset();
-    //        for( auto i : c ) count += i;
-    //        Cout( "std::list foreach ms: ", sw.elapsedMillseconds() );
-    //        Cout( count );
-
-    //        count = 0;
-    //        sw.reset();
-    //        for( int i = 0; i < 99999; ++i )
-    //        {
-    //            auto it = find( c.begin(), c.end(), i );
-    //            if( it != c.end() )
-    //            {
-    //                count += ( *it );
-    //            }
-    //        }
-    //        Cout( "std::list find ms: ", sw.elapsedMillseconds() );
-    //        Cout( count );
-
-    //        vector<decltype( c.begin() )> its;
-    //        for( auto it = c.begin(); it != c.end(); ++it ) its.push_back( it );
-    //        sw.reset();
-    //        for( auto it : its ) c.erase( it );
-    //        Cout( "std::list erase ms: ", sw.elapsedMillseconds() );
-    //        Cout( count );
-    //    }
-
-    //    count = 0;
-    //    {
-    //        Links<int> c;
-    //        Stopwatch sw;
-    //        for( int i = 0; i < 9999999; ++i )
-    //        {
-    //            c.insert( i );
-    //        }
-    //        Cout( "Links insert ms: ", sw.elapsedMillseconds() );
-    //        Cout( c.size() );
-
-    //        count = 0;
-    //        sw.reset();
-    //        for( int i = 0; i < 9999999; ++i ) count += c[ i ]->value;
-    //        Cout( "Links foreach ms: ", sw.elapsedMillseconds() );
-    //        Cout( count );
-
-    //        count = 0;
-    //        sw.reset();
-    //        for( int i = 0; i < 99999; ++i )
-    //        {
-    //            auto it = c.find( i );
-    //            if( it ) count += it->value;
-    //        }
-    //        Cout( "Links find ms: ", sw.elapsedMillseconds() );
-    //        Cout( count );
-
-    //        vector<decltype( c.insert( 0 ) )> its;
-    //        for( int i = 0; i < 9999999; ++i ) its.push_back( c[ i ] );
-    //        sw.reset();
-    //        for( auto it : its ) c.erase( it );
-    //        Cout( "Links erase ms: ", sw.elapsedMillseconds() );
-    //        Cout( count );
-    //    }
-
-    //    Cout( "\n\n" );
-    //}
+    int alive = foos.size();
+    while( alive )
+    {
+        for( int i = foos.size() - 1; i >= 0; --i )
+        {
+            auto& foo = foos[ i ];
+            if( !foo->process() )
+            {
+                foos.top()->idx = i;
+                foos.eraseFast( i );
+                delete foo;
+                --alive;
+            }
+        }
+    }
 
     system( "pause" );
     return 0;
 }
-
