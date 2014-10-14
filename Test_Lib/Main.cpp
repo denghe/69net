3f6ec581432f461ba958f56f52ef2663945be314
denghe <denghe@hotmail.com>
2014-10-14 23:30:22 +0800

    cor manager

--- Test_Lib/Main.cpp
+++ Test_Lib/Main.cpp
@@ -9,38 +9,38 @@ using namespace std;
 
 // todo: message ?
 
-struct FooBaseManager;
-struct FooBase
+template<typename T>
+struct AutoID
 {
-    FooBaseManager* manager;
-
-    //static const int typeId = xxx;
-    virtual int GetTypeId() = 0;    // return typeId;
-
-    // void Init( ... );
-    virtual void Destroy() {}
-    virtual ~FooBase() {}
-
-    int id = 0, idx = 0, ln = 0, sleeps = 0;
-    virtual bool Process() = 0;
-    virtual bool Sleeping() { --sleeps; return true; }
+    int value;
+    static int maxValue;
+    AutoID() { value = maxValue++; }
 };
+template<typename T>
+int AutoID<T>::maxValue = 0;
 
-struct FooBaseManager
+
+template<typename T>
+struct CorManager
 {
-    List<FooBase*> items;
-    Dict<int, List<FooBase*>> avaliables;
-    int aiid = 0;
+    List<T*> items;
+    List<List<T*>> pool;
 
+    CorManager()
+    {
+        pool.resize( AutoID<T>::maxValue + 1 );
+    }
+
+    int aiid = 0;
     template<typename T, typename ...PTS>
     T* CreateItem( PTS ...parms )
     {
-        static_assert( std::is_base_of<FooBase, T>::value, "T must be inherit from FooBase*" );
+        static_assert( std::is_base_of<T, T>::value, "T must be inherit from FooBase*" );
         T* rtv;
-        auto& pool = avaliables[ T::typeId ];
-        if( pool.size() )
+        auto& objs = pool[ T::typeId.value ];
+        if( objs.size() )
         {
-            rtv = (T*)avaliables[ T::typeId ].top_pop();
+            rtv = (T*)objs.top_pop();
         }
         else
         {
@@ -56,22 +56,22 @@ struct FooBaseManager
         return rtv;
     }
 
-    void DestroyItem( FooBase* foo )
+    void DestroyItem( T* foo )
     {
         items.top()->idx = foo->idx;
         items.eraseFast( foo->idx );
         foo->Destroy();
         foo->id = 0;
-        avaliables[ foo->GetTypeId() ].push( foo );
+        pool[ foo->GetTypeId() ].push( foo );
     }
 
-    bool Process()
+    bool Process( int ticks = 0 )
     {
         if( !items.size() ) return false;
         for( int i = items.size() - 1; i >= 0; --i )
         {
             auto& foo = items[ i ];
-            if( !foo->Process() ) DestroyItem( foo );
+            if( !foo->Process( ticks ) ) DestroyItem( foo );
         }
         return true;
     }
@@ -87,71 +87,113 @@ struct FooBaseManager
 
     void Compress()
     {
-        for( int i = avaliables.size() - 1; i >= 0; --i )
+        for( int i = 0; i < items.size(); ++i )
         {
-            auto& pool = avaliables.data()[ i ]->value;
+            items[ i ]->EnsureRefs();
+        }
+        for( int i = pool.size() - 1; i >= 0; --i )
+        {
+            auto& objs = pool[ i ];
             for( int j = 0; j < pool.size(); ++j )
             {
-                delete pool[ j ];
+                delete objs[ j ];
             }
             pool.clear();
         }
     }
 };
 
-struct Foo : public FooBase
+
+
+struct Cor
 {
-    static const int typeId = 123;
-    virtual int GetTypeId() { return typeId; }
+    CorManager<Cor>* manager;
 
-    Foo() { Cout( "new Foo" ); }
+    // static const AutoID<Base1> typeId;
+    // int GetTypeId() override { return typeId.value; }
+    virtual int GetTypeId() = 0;
 
-    int i;
+    // void Init( ... );
+    inline virtual void EnsureRefs() {}
+    inline virtual void Destroy() {}
+    virtual ~Cor() {}
+
+    int id = 0, idx = 0, ln = 0, sleeps = 0;
+    virtual bool Process( int ticks ) = 0;
+    inline virtual bool Sleeping() { --sleeps; return true; }
+};
+
+
+
+template<typename T>
+struct CorBase : public Cor
+{
+    static const AutoID<Cor> typeId;
+    int GetTypeId() override { return typeId.value; }
+};
+template<typename T>
+const AutoID<Cor> CorBase<T>::typeId;
+
+
+
+
+#define REF_DECL( T, N ) T* N; int N##Id
+#define REF_SET( N, P )  N = P; N##Id = P->id
+#define REF_ENSURE( N )  if( N && N->id != N##Id ) N = nullptr
+#define REF_CLEAR( N )   N = nullptr
+
+struct Bar : public CorBase<Bar>
+{
     String txt;
-
     void Init( String _txt )
     {
         txt = _txt;
     }
-
-    virtual bool Process() override
+    bool Process( int _ticks ) override
     {
         COR_BEGIN;
-        for( i = 0; i < 3; ++i )
-        {
-            Cout( txt, i );
-            COR_YIELD;
-            COR_SLEEP( 2 );
-        }
+        Cout( txt, " say: i'm living..." );
+        COR_SLEEP( 5 );
+        Cout( txt, " say: i'm dieing..." );
         COR_END;
     }
 };
 
+struct Foo : public CorBase<Foo>
+{
+    REF_DECL( Bar, bar );
+
+    void Init( Bar* _bar )
+    {
+        REF_SET( bar, _bar );
+    }
+    void EnsureRefs() override
+    {
+        REF_ENSURE( bar );
+    }
+
+    int i;
+    bool Process( int _ticks ) override
+    {
+        EnsureRefs();
+        COR_BEGIN;
+        for( i = 0; i < 9; ++i )
+        {
+            Cout( "Foo's i = ", i );
+            if( bar )
+            {
+                Cout( "Foo's Bar alive!" );
+            }
+            COR_YIELD;
+        }
+        COR_END;
+    }
+};
 int main()
 {
-    FooBaseManager fbm;
-    fbm.CreateItem<Foo>( "foo1: " );
-    fbm.CreateItem<Foo>( "foo2: " );
+    CorManager<Cor> fbm;
+    fbm.CreateItem<Foo>( fbm.CreateItem<Bar>( "Bar" ) );
     while( fbm.Process() );
-    fbm.CreateItem<Foo>( "foo1: " );
-    fbm.CreateItem<Foo>( "foo2: " );
-    while( fbm.Process() );
-
-    //Foo f;
-    //for( int i = 0; f.Process(); ++i ) Cout( "i = ", i );
-
-    /*
-        List<FooBase*> foos;
-        for( int i = 0; i < 3; ++i )
-        {
-        auto foo = new Foo();
-        foo->idx = foos.size();
-        foos.push( foo );
-        }
-        int alive = foos.size();
-        while( alive )
-        {
-        }*/
 
     system( "pause" );
     return 0;
