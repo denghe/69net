a6c1c683f40c7f380c9812a5303fda962326cace
denghe <denghe@hotmail.com>
2015-06-20 22:25:51 +0800

    put some lua test code here

--- Lib/Singleton.h
+++ Lib/Singleton.h
@@ -1,75 +1,66 @@
 #ifndef __SINGLETON_H__
 #define __SINGLETON_H__
 
-
 namespace xxx
 {
-    //template<typename T>
-    //struct Singleton
-    //{
-    //protected:
-    //    static T* instance;
-
-    //    Singleton() = default;
-    //    Singleton( const Singleton & ) = delete;
-    //    Singleton &operator =( const Singleton & ) = delete;
-
-    //public:
-    //    static T* GetInstance()
-    //    {
-    //        return instance;
-    //    }
-    //    template<typename ...PTS>
-    //    static void InitInstance( PTS && ... ps )
-    //    {
-    //        assert( !instance );
-    //        instance = new T( std::forward<PTS>( ps )... );
-    //    }
-    //    static void DestroyInstance()
-    //    {
-    //        assert( instance );
-    //        delete instance;
-    //        instance = nullptr;
-    //    }
-    //};
-    //template<typename T>
-    //T* Singleton<T>::instance = nullptr;
-
-
-    // 这是会在程序退出时自动析构的实现( 用 unique_ptr 包了一下 instance )
+#ifdef SINGLETON_DISABLE_AUTO_DESTROY
     template<typename T>
-    struct Singleton
+    class Singleton
     {
-    protected:
-        static std::unique_ptr<T>& GetInstanceCore()
-        {
-            static std::unique_ptr<T> instance;
-            return instance;
-        }
-
+        static T* instance;
         Singleton() = default;
-        Singleton( const Singleton& ) = delete;
-        Singleton &operator =( const Singleton& ) = delete;
-
+        Singleton( const Singleton & ) = delete;
+        Singleton &operator =( const Singleton & ) = delete;
     public:
         static T* GetInstance()
         {
-            assert( GetInstanceCore() != nullptr );
-            return GetInstanceCore().get();
+            assert( instance );
+            return instance;
+        }
+        template<typename ...PTS>
+        static void InitInstance( PTS && ... ps )
+        {
+            assert( !instance );
+            instance = new T( std::forward<PTS>( ps )... );
+        }
+        static void DestroyInstance()
+        {
+            assert( instance );
+            delete instance;
+            instance = nullptr;
+        }
+    };
+    template<typename T>
+    T* Singleton<T>::instance = nullptr;
+#else
+    template<typename T>
+    class Singleton
+    {
+        static std::unique_ptr<T> instance;
+        Singleton() = default;
+        Singleton( const Singleton& ) = delete;
+        Singleton &operator =( const Singleton& ) = delete;
+    public:
+        static T* GetInstance()
+        {
+            assert( instance );
+            return instance.get();
         }
         template<typename ...PTS>
         static void InitInstance( PTS&& ... ps )
         {
-            assert( GetInstanceCore() == nullptr );
-            GetInstanceCore().reset( new T( std::forward<PTS>( ps )... ) ); // todo: new T{} 这样写可兼容初始化列表 ?
+            assert( !instance );
+            instance.reset( new T( std::forward<PTS>( ps )... ) );
         }
         static void DestroyInstance()
         {
-            assert( GetInstanceCore() != nullptr );
-            GetInstanceCore().reset();
+            assert( instance );
+            instance.reset();
         }
     };
+    template<typename T>
+    std::unique_ptr<T> Singleton<T>::instance;
+#endif
 }
-
 
 #endif
--- Switch/Main.cpp
+++ Switch/Main.cpp
@@ -1,240 +1,107 @@
 #include "Lib/All.h"
 
-inline void w7( char * buf, int & offset, uint32 v )
-{
-Lab1:
-    auto b7 = uint8( v );
-    if( ( v >>= 7 ) )
-    {
-        buf[ offset++ ] = b7 | 0x80u;
-        goto Lab1;
-    }
-    buf[ offset++ ] = b7;
-}
-
-inline void r7( uint32 & v, char * buf, int & offset )
-{
-    auto p = buf + offset;
-    uint i = 0, b7, lshift = 0, idx = 0;
-Lab1:
-    b7 = p[ idx++ ];
-    if( b7 > 0x7F )
-    {
-        i |= ( b7 & 0x7F ) << lshift;
-        lshift += 7;
-        goto Lab1;
-    }
-    else i |= b7 << lshift;
-    v = i;
-    offset += idx;
-}
-
-inline void w72( char * buf, int & offset, uint32 v )
-{
-    if( v <= 0x7Fu )
-    {
-        buf[ offset++ ] = v;
-    }
-    else if( v <= 0x3FFFu )
-    {
-        buf[ offset ] = v | 0x80u;
-        buf[ offset + 1 ] = v >> 7;
-        offset += 2;
-    }
-    else if( v <= 0x1FFFFFu )
-    {
-        buf[ offset ] = v | 0x80u;
-        buf[ offset + 1 ] = ( v >> 7 ) | 0x80u;
-        buf[ offset + 2 ] = v >> 14;
-        offset += 3;
-    }
-    else if( v <= 0xFFFFFFFu )
-    {
-        buf[ offset ] = v | 0x80u;
-        buf[ offset + 1 ] = ( v >> 7 ) | 0x80u;
-        buf[ offset + 2 ] = ( v >> 14 ) | 0x80u;
-        buf[ offset + 3 ] = v >> 21;
-        offset += 4;
-    }
-    else
-    {
-        buf[ offset ] = v | 0x80u;
-        buf[ offset + 1 ] = ( v >> 7 ) | 0x80u;
-        buf[ offset + 2 ] = ( v >> 14 ) | 0x80u;
-        buf[ offset + 3 ] = ( v >> 21 ) | 0x80u;
-        buf[ offset + 4 ] = v >> 28;
-        offset += 5;
-    }
-}
-
 using namespace std;
 using namespace xxx;
+
+#include "lua.hpp"
+enum class LuaDataTypes
+{
+    NoValue, Nil, Boolean, UserData, Number, String, Table, Function, Thread, Proto
+};
+const LuaDataTypes map_lua_type_enums[] = {
+    LuaDataTypes::NoValue,
+    LuaDataTypes::Nil, LuaDataTypes::Boolean, LuaDataTypes::UserData, LuaDataTypes::Number,
+    LuaDataTypes::String, LuaDataTypes::Table, LuaDataTypes::Function, LuaDataTypes::UserData, LuaDataTypes::Thread,
+    LuaDataTypes::Proto
+};
+struct Lua
+{
+    lua_State* L = nullptr;
+    String errMsg;
+    Lua() { L = luaL_newstate(); }
+    ~Lua() { lua_close( L ); }
+    inline bool LoadString( String const& code )
+    {
+        auto rtv = luaL_loadstring( L, code.C_str() );
+        // todo if rtv errMsg = ....
+        return rtv == 0;
+    }
+    inline bool PCall()
+    {
+        auto rtv = lua_pcall( L, 0, LUA_MULTRET, 0 );
+        // todo if rtv errMsg = ....
+        return rtv == 0;
+    }
+    inline int GetTop()
+    {
+        return lua_gettop( L );
+    }
+    inline void SetTop( int idx )
+    {
+        lua_settop( L, idx );
+    }
+    inline LuaDataTypes GetType( int idx )
+    {
+        return map_lua_type_enums[ lua_type( L, idx ) + 1 ];
+    }
+    inline const char* GetTypeName( int idx )
+    {
+        return lua_typename( L, lua_type( L, idx ) );
+    }
+    inline bool IsInteger( int idx )
+    {
+        return lua_isinteger( L, idx ) == 1;
+    }
+    inline String GetString( int idx, bool ref = false )
+    {
+        size_t len;
+        auto rtv = lua_tolstring( L, idx, &len );
+        return String( rtv, len, len, ref );
+    }
+    inline lua_Number GetNumber( int idx )
+    {
+        return lua_tonumber( L, idx );
+    }
+    inline lua_Integer GetInteger( int idx )
+    {
+        return lua_tointeger( L, idx );
+    }
+
+    // todo: http://blog.codingnow.com/2015/05/lua_c_api.html
+};
+
 int main()
 {
-    ByteBuffer fb;
-    uint count = 0xFFFFFFFF;
-
-    char buf[ 16 ];
-    int offset = 0;
-    uint forRead = 0;
-
-    Stopwatch sw;
-    for( uint i = 0; i < count; ++i )
+    Lua L;
+    L.LoadString( R"--(
+    return "asdf", 12, 34.5
+)--" );
+    L.PCall();
+    auto dump = [ &]( int idx )
     {
-        offset = 0;
-        w72( buf, offset, i );
-        //offset = 0;
-        //r7( forRead, buf, offset );
+        switch( L.GetType( idx ) )
+        {
+        case LuaDataTypes::String:
+            CoutLine( L.GetString( idx ) );
+            break;
+        case LuaDataTypes::Number:
+            if( L.IsInteger( idx ) )
+            {
+                CoutLine( L.GetInteger( idx ) );
+            }
+            else
+            {
+                CoutLine( L.GetNumber( idx ) );
+            }
+        }
+    };
+    for( int i = 1; i <= L.GetTop(); ++i )
+    {
+        Cout( "data type = ", L.GetTypeName( i ), "; value = " );
+        dump( i );
     }
-    Cout( sw.ElapsedMillseconds(), "ms" );
-    offset = 0;
-    r7( forRead, buf, offset );
-    Cout( forRead );
-
-
-
-
-
-
-    //for( int i = 0; i < count; ++i )
-    //{
-    //    fb.write( i );
-    //}
-    //fb.clear();
-    //Stopwatch sw;
-    //for( int i = 0; i < count; ++i )
-    //{
-    //    fb.write( i );
-    //}
-    //Cout( "write ", count, " i to fb, elapsed ", sw.elapsedMillseconds(), "ms" );
-    //Cout( fb.size() );
-
-
-    //PacketTestPackets::Obj_t3 t3;
-    //// ‘§»»
-    //for( int i = 0; i < count; ++i )
-    //{
-    //    fb.write( t3 );
-    //}
-
-    //fb.clear();
-    //Stopwatch sw;
-    //for( int i = 0; i < count; ++i )
-    //{
-    //    fb.write( t3 );
-    //}
-    //Cout( "write ", count, " t3 to fb, elapsed ", sw.elapsedMillseconds(), "ms" );
-    //Cout( fb.size() );
-
-    //sw.reset();
-    //for( int i = 0; i < count; ++i )
-    //{
-    //    fb.read( t3 );
-    //}
-    //Cout( "read ", count, " t3 from fb, elapsed ", sw.elapsedMillseconds(), "ms" );
-    //Cout( fb.offset() );
-
-
-    //int count = 999999;
-    //Pool p( 32 );
-    //vector<String> ss;
-    //vector<string> stdss;
-    //// ‘§»»
-    //for( int i = 0; i < count; ++i )
-    //{
-    //    ss.emplace_back( p );
-    //    stdss.emplace_back();
-    //}
-
-    //ss.clear();
-    //Stopwatch sw;
-    //for( int i = 0; i < count; ++i )
-    //{
-    //    ss.emplace_back( "asdfasdfasdfasdfasdf" );
-    //}
-    //Cout( "List push new String: ", sw.elapsedMillseconds() );
-
-    //ss.clear();
-
-    //sw.reset();
-    //for( int i = 0; i < count; ++i )
-    //{
-    //    ss.emplace_back( p, "asdfasdfasdfasdfasdf" );
-    //}
-    //Cout( "List push pool String: ", sw.elapsedMillseconds() );
-
-    //stdss.clear();
-    //sw.reset();
-    //for( int i = 0; i < count; ++i )
-    //{
-    //    stdss.emplace_back( "asdfasdfasdfasdfasdf" );
-    //}
-    //Cout( "List push empty std::string: ", sw.elapsedMillseconds() );
-
-
-    //{
-    //    Dict<String, String> d;
-    //    Stopwatch sw;
-    //    Pool p( 128 );
-    //    for( int i = 0; i < 999999; ++i )
-    //    {
-    //        d.insert( String::make( p, "asdfqwerasdfqwerasdfqwer", i ), String::make( p, "asdfqwerasdfqwerasdfqwer", i ) );
-    //    }
-    //    Cout( sw.elapsedMillseconds() );
-    //    Cout( d[ "asdfqwerasdfqwerasdfqwer99998" ] );
-    //    sw.reset();
-    //    d.clear();
-    //    Cout( sw.elapsedMillseconds() );
-    //}
-
-    //{
-    //    unordered_map<string, string> d;
-    //    Stopwatch sw;
-    //    for( int i = 0; i < 999999; ++i )
-    //    {
-    //        d.insert( make_pair( "asdfqwerasdfqwerasdfqwer" + to_string( i ), "asdfqwerasdfqwerasdfqwer" + to_string( i ) ) );
-    //    }
-    //    cout << sw.elapsedMillseconds() << endl;
-    //    cout << d[ "asdfqwerasdfqwerasdfqwer99998" ] << endl;
-    //    sw.reset();
-    //    d.clear();
-    //    cout << sw.elapsedMillseconds() << endl;
-    //}
+    L.SetTop( 0 );
 
     system( "pause" );
     return 0;
 }
-
-
-
-//#include "Lib/All.h"
-//
-//int main()
-//{
-//    static const int tc = 100000000;
-//    Pool pool( 4 );
-//    std::cout << "Time: ";
-//    Stopwatch sw;
-//    for( int i = 0; i < tc; ++i )
-//    {
-//        void * p1 = pool.alloc();
-//        void * p2 = pool.alloc();
-//        void * p3 = pool.alloc();
-//        void * p4 = pool.alloc();
-//        void * p5 = pool.alloc();
-//        void * p6 = pool.alloc();
-//        void * p7 = pool.alloc();
-//        void * p8 = pool.alloc();
-//        pool.free( p1 );
-//        pool.free( p2 );
-//        pool.free( p3 );
-//        pool.free( p4 );
-//        pool.free( p5 );
-//        pool.free( p6 );
-//        pool.free( p7 );
-//        pool.free( p8 );
-//    }
-//    std::cout << sw.elapsedMillseconds() << "s" << std::endl;
-//    system( "pause" );
-//    return 0;
-//}
